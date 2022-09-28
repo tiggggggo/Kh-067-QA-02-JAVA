7dd4c8f1309e54a188d479db0d7503a3a94dc29e
Vika <Vikamurzenko@gmail.com>
2022-09-28 18:55:47 +0100

    Created folder with FileUtils class with methods

--- Kh-067-QA-02-JAVA/src/main/java/Main.java
+++ Kh-067-QA-02-JAVA/src/main/java/Main.java
@@ -1,5 +1,17 @@
+import utils.FileUtils;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        boolean result = FileUtils.createIfNotExists("test.txt");
+        System.out.println(result);
 
+        List<String> resultList = FileUtils.readFile("test.txt");
+        System.out.println(resultList);
+
+        FileUtils.writeFile("test.txt", Collections.singletonList("Hello"));
     }
 }
