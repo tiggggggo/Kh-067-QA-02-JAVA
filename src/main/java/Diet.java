55e01b105634fd2b96f729d02ad6243a414f9a0d
Nikita Katerynin <rinanor505@gmail.com>
2022-10-01 21:37:17 +0300

    Changes to file Diet.java were added

--- src/main/java/Diet.java
+++ src/main/java/Diet.java
@@ -3,21 +3,20 @@ import static utils.DateUtils.getDateAsString;
 import java.util.*;
 
 public class Diet {
-    LocalDate localDate;
-    List<Product> productList;
-    void addProduct(Product product){
+    private LocalDate localDate;
+    private List<Product> productList;
+    public void addProduct(Product product){
         if (localDate.isEqual(LocalDate.now())){
             productList.add(product);
+
+        }
+        else {
+            System.out.println("Entered date is wrong. Please, select today's date.");
         }
     }
 
     public Diet(List<Product> productList, LocalDate localDate){
         this.localDate = localDate;
         this.productList = productList;
-        System.out.println("For "+ getDateAsString(localDate, "yyyy-MM-dd") + " was eaten: ");
-        for (Product pr: productList) {
-            System.out.println(pr.getName() + pr.getWeight() + "gr");
-        }
     }
-
 }
