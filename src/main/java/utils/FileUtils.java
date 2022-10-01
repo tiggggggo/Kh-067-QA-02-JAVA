4993b1279c28c8e54df47b43b1ea15f000945749
Nikita Katerynin <rinanor505@gmail.com>
2022-10-01 15:19:51 +0300

    New file FileUtils.java was added

---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,45 @@
+package utils;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+
+    public static boolean createIfNotExists(String filePath)  {
+
+        File myObj = new File(filePath);
+        try {
+            return myObj.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public static List<String> readFile(String filePath){
+        List<String> result = new ArrayList<>();
+        try (BufferedReader buffer = new BufferedReader(
+                new FileReader(filePath))) {
+            String fileString;
+            while ((fileString = buffer.readLine()) != null) {
+                result.add(fileString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+
+    public static void writeFile(String filePath, List<String> listOfWords){
+        try (BufferedWriter buffer = new BufferedWriter(
+                new FileWriter(filePath,true))) {
+            for (String word : listOfWords) {
+                buffer.write(word+"\n");
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
